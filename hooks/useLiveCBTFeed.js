77efb5242c194304964153ed2e3d226ff00407fe
CBT Setup <setup@cbt.local>
2026-05-08 15:41:21 +0100

    Phase 22: WebSocket Real-Time CBT Intelligence Layer

---
+++ hooks/useLiveCBTFeed.js
@@ -0,0 +1,18 @@
+﻿import { useEffect, useState } from 'react'
+
+export function useLiveCBTFeed() {
+  const [events, setEvents] = useState([])
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080')
+
+    ws.onmessage = (msg) => {
+      const data = JSON.parse(msg.data)
+      setEvents(prev => [data, ...prev])
+    }
+
+    return () => ws.close()
+  }, [])
+
+  return events
+}
---
+++ server/websocketServer.js
@@ -0,0 +1,28 @@
+﻿const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 8080 })
+
+const clients = []
+
+wss.on('connection', (ws) => {
+  clients.push(ws)
+
+  ws.on('message', (message) => {
+    console.log('Received:', message)
+  })
+
+  ws.on('close', () => {
+    const index = clients.indexOf(ws)
+    if (index > -1) clients.splice(index, 1)
+  })
+})
+
+function broadcast(data) {
+  clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data))
+    }
+  })
+}
+
+module.exports = { broadcast }
